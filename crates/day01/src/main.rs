09536eff05d433e2736e2f6378a85dee128017d8
Lily Richards <lilymichellerichards@gmail.com>
2025-12-01 10:06:47 -0500

    Day 1 part 2

--- crates/day01/src/main.rs
+++ crates/day01/src/main.rs
@@ -1,31 +1,30 @@
 use std::fs::read_to_string;
 
-fn parse_line_part1(line: &str) -> i8 {
-    let value = line[1..].parse::<i32>().unwrap();
-    if line.starts_with('L') {
-        -(value % 100) as i8
+fn parse_line(line: &str) -> i64 {
+    let value = line[1..].parse::<i64>().unwrap();
+    if line.starts_with('L') { -value } else { value }
+}
+
+fn parse_input(input: &str) -> Vec<i64> {
+    input.lines().map(parse_line).collect()
+}
+
+fn next_position(cur: i64, rotation: i64) -> i64 {
+    let next = (cur + rotation) % 100;
+    if next < 0 { next + 100 } else { next }
+}
+
+fn passes_zero_count(cur: i64, rotation: i64) -> i64 {
+    let passes = ((cur + rotation) / 100).abs();
+    if cur + rotation <= 0 && cur != 0 {
+        passes + 1
     } else {
-        (value % 100) as i8
+        passes
     }
 }
 
-fn parse_input_part1(input: &str) -> Vec<i8> {
-    input.lines().map(parse_line_part1).collect()
-}
-
-fn next_position(cur: u8, rotation: i8) -> u8 {
-    let next: i16 = cur as i16 + rotation as i16;
-    if next < 0 {
-        (next + 100) as u8
-    } else if next >= 100 {
-        (next - 100) as u8
-    } else {
-        next as u8
-    }
-}
-
-fn part1(input: &str) -> u32 {
-    let rotations = parse_input_part1(input);
+fn part1(input: &str) -> i64 {
+    let rotations = parse_input(input);
     let mut position = 50;
     let mut answer = 0;
     for rotation in rotations {
@@ -37,15 +36,28 @@ fn part1(input: &str) -> u32 {
     answer
 }
 
+fn part2(input: &str) -> i64 {
+    let rotations = parse_input(input);
+    let mut position = 50;
+    let mut answer = 0;
+    for rotation in rotations {
+        answer += passes_zero_count(position, rotation);
+        position = next_position(position, rotation);
+    }
+    answer
+}
+
 fn main() {
     let input = read_to_string("input.txt").unwrap();
     let p1_answer = part1(&input);
     println!("day 1 part 1: {}", p1_answer);
+    let p2_answer = part2(&input);
+    println!("day 1 part 2: {}", p2_answer);
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::part1;
+    use crate::{part1, part2};
 
     #[test]
     fn basic_test_part1() {
@@ -61,5 +73,21 @@ R114
 L182";
         let final_position = part1(basic_input);
         assert_eq!(final_position, 3);
+    }
+
+    #[test]
+    fn basic_test_part2() {
+        let basic_input = "L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82";
+        let count = part2(basic_input);
+        assert_eq!(count, 6);
     }
 }
