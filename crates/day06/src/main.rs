956e5eb7b374197bae657d982279e0fe631b9bf9
Lily Richards <lilymichellerichards@gmail.com>
2025-12-06 11:00:19 -0500

    Day 6 part 2

--- crates/day06/src/main.rs
+++ crates/day06/src/main.rs
@@ -72,10 +72,8 @@ fn parse_input_part1(input: &str) -> Input {
 
 fn part1(input: &str) -> u64 {
     let input = parse_input_part1(input);
-    println!("input {:?}", &input);
     let mut total = 0;
     for (col, op) in input.ops.iter().enumerate() {
-        println!("col {} op {:?}", col, op);
         let fold_fn = op.fold_fn();
         let start_val = op.acc_init();
         let col_result = input
@@ -83,7 +81,6 @@ fn part1(input: &str) -> u64 {
             .iter()
             .map(|row| row[col])
             .fold(start_val, fold_fn);
-        println!("col result {}", col_result);
         total += col_result;
     }
     total
@@ -94,26 +91,89 @@ struct Part2Problem {
     op: Operator,
 }
 
+fn parse_digits(digits: &[u8]) -> Option<u64> {
+    if digits.iter().all(|d| *d == b' ') {
+        None
+    } else {
+        Some(
+            digits
+                .iter()
+                .filter(|d| **d != b' ')
+                .fold(0, |acc, d| acc * 10 + (d - b'0') as u64),
+        )
+    }
+}
+
+fn get_numbers(input: &str) -> Vec<Option<u64>> {
+    let mut lines: Vec<&[u8]> = input
+        .lines()
+        .map(|line| line.as_bytes())
+        .rev()
+        .skip(1)
+        .collect();
+    lines.reverse();
+    let mut numbers: Vec<Vec<u8>> = vec![];
+    for col in 0..lines[0].len() {
+        let cur_num = lines.iter().map(|line| line[col]).collect();
+        numbers.push(cur_num);
+    }
+    numbers
+        .into_iter()
+        .map(|digits| parse_digits(&digits))
+        .collect()
+}
+
 fn parse_input_part2(input: &str) -> Vec<Part2Problem> {
-    todo!()
+    let numbers = get_numbers(input);
+    let ops: Vec<Operator> = input
+        .lines()
+        .last()
+        .unwrap()
+        .split_whitespace()
+        .map(|s| s.parse::<Operator>().unwrap())
+        .collect();
+    let mut cur_numbers = vec![];
+    let mut result = vec![];
+    let mut op_idx = 0;
+    for number in numbers {
+        if let Some(num) = number {
+            cur_numbers.push(num);
+        } else {
+            result.push(Part2Problem {
+                nums: cur_numbers,
+                op: ops[op_idx],
+            });
+            cur_numbers = vec![];
+            op_idx += 1;
+        }
+    }
+    result.push(Part2Problem {
+        nums: cur_numbers,
+        op: ops[op_idx],
+    });
+    result
 }
 
 fn part2(input: &str) -> u64 {
     let input = parse_input_part2(input);
-    todo!()
+    input
+        .into_iter()
+        .map(|prob| {
+            prob.nums
+                .into_iter()
+                .fold(prob.op.acc_init(), prob.op.fold_fn())
+        })
+        .sum()
 }
 
 fn main() {
     let input = read_to_string("input.txt").unwrap();
-    let input = input.trim();
     let p1_answer = part1(&input);
     assert_ne!(p1_answer, 5171058832173);
     assert!(p1_answer > 5171058832173);
     println!("day 1 part 1: {}", p1_answer);
-    /*
     let p2_answer = part2(&input);
     println!("day 1 part 2: {}", p2_answer);
-    */
 }
 
 #[cfg(test)]
@@ -135,10 +195,8 @@ mod tests {
         assert_eq!(part1(DERIVED_INPUT), 394357749);
     }
 
-    /*
     #[test]
     fn basic_test_part2() {
         assert_eq!(part2(BASIC_INPUT), 3263827);
     }
-    */
 }
