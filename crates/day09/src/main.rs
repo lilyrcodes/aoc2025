5fd251e5938d7543e51e31b78211775679b15ab6
Lily Richards <lilymichellerichards@gmail.com>
2025-12-10 14:58:10 -0500

    Fuck day 9 part 2 I'm skipping it

--- crates/day09/src/main.rs
+++ crates/day09/src/main.rs
@@ -1,4 +1,7 @@
-use std::fs::read_to_string;
+use std::{
+    collections::{HashMap, HashSet, VecDeque},
+    fs::read_to_string,
+};
 
 #[derive(PartialEq, Eq, PartialOrd, Ord, Hash, Debug, Clone, Copy)]
 struct Coord {
@@ -6,9 +9,16 @@ struct Coord {
     y: usize,
 }
 
+#[derive(PartialEq, Eq, PartialOrd, Ord, Hash, Debug, Clone, Copy)]
+enum Tile {
+    UNKNOWN,
+    VALID,
+    INVALID,
+}
+
 #[derive(PartialEq, Eq, PartialOrd, Debug, Clone)]
 struct TileGrid {
-    tiles: Vec<bool>,
+    tiles: Vec<Tile>,
     largest_x: usize,
     largest_y: usize,
 }
@@ -46,19 +56,27 @@ fn tile_index(tiles: &TileGrid, x: usize, y: usize) -> usize {
     x + (tiles.largest_x + 1) * y
 }
 
-fn get_tile(tiles: &TileGrid, x: usize, y: usize) -> bool {
+fn get_tile(tiles: &TileGrid, x: usize, y: usize) -> Tile {
     tiles.tiles[tile_index(tiles, x, y)]
 }
 
-fn all_set(tiles: &TileGrid, min_x: usize, max_x: usize, y: usize) -> bool {
-    tiles.tiles[tile_index(tiles, min_x, y)..=tile_index(tiles, max_x, y)]
+fn all_set(tiles: &TileGrid, bools: &[bool], min_x: usize, max_x: usize, y: usize) -> bool {
+    bools[tile_index(tiles, min_x, y)..=tile_index(tiles, max_x, y)]
         .iter()
-        .all(|t| *t)
+        .all(|b| *b)
 }
 
-fn set_tile(tiles: &mut TileGrid, x: usize, y: usize) {
+fn to_bools(tiles: &TileGrid) -> Vec<bool> {
+    tiles
+        .tiles
+        .iter()
+        .map(|t| *t == Tile::VALID || *t == Tile::UNKNOWN)
+        .collect()
+}
+
+fn set_tile(tiles: &mut TileGrid, x: usize, y: usize, tile: Tile) {
     let idx = tile_index(tiles, x, y);
-    tiles.tiles[idx] = true
+    tiles.tiles[idx] = tile
 }
 
 fn green_line(tiles: &mut TileGrid, prev: &Coord, coord: &Coord) {
@@ -66,26 +84,79 @@ fn green_line(tiles: &mut TileGrid, prev: &Coord, coord: &Coord) {
         let from = prev.x.min(coord.x);
         let to = prev.x.max(coord.x);
         for x in from..=to {
-            set_tile(tiles, x, prev.y);
+            set_tile(tiles, x, prev.y, Tile::VALID);
         }
     } else {
         let from = prev.y.min(coord.y);
         let to = prev.y.max(coord.y);
         for y in from..=to {
-            set_tile(tiles, prev.x, y);
+            set_tile(tiles, prev.x, y, Tile::VALID);
         }
     }
 }
 
-// Doesn't work, need a better way to check if coord is in the polygon
+fn is_explored(tiles: &TileGrid, explored: &[bool], x: usize, y: usize) -> bool {
+    explored[tile_index(tiles, x, y)]
+}
+
+fn set_explored(tiles: &TileGrid, explored: &mut [bool], x: usize, y: usize) {
+    explored[tile_index(tiles, x, y)] = true
+}
+
 fn fill_grid(tiles: &mut TileGrid) {
-    for y in 0..=tiles.largest_y {
-        let mut on_green = false;
-        for x in 0..=tiles.largest_x {
-            if get_tile(tiles, x, y) {
-                on_green = !on_green;
-            } else if on_green {
-                set_tile(tiles, x, y);
+    let mut queue: VecDeque<Coord> = VecDeque::new();
+    let mut explored: Vec<bool> = vec![false; (tiles.largest_x + 2) * (tiles.largest_y + 2)];
+    queue.push_back(Coord { x: 0, y: 0 });
+    let mut count: usize = 0;
+    while let Some(coord) = queue.pop_front() {
+        if count % 1_000_000 == 999_999 {
+            println!("{} / {}", count / 1_000_000, explored.len() / 1_000_000);
+        }
+        if is_explored(tiles, &explored, coord.x, coord.y) {
+            continue;
+        }
+        if get_tile(tiles, coord.x, coord.y) == Tile::VALID {
+            count += 1;
+            set_explored(tiles, &mut explored, coord.x, coord.y);
+            continue;
+        }
+        set_tile(tiles, coord.x, coord.y, Tile::INVALID);
+        set_explored(tiles, &mut explored, coord.x, coord.y);
+        count += 1;
+        if coord.x != 0 {
+            let left = Coord {
+                x: coord.x - 1,
+                y: coord.y,
+            };
+            if !is_explored(tiles, &explored, left.x, left.y) {
+                queue.push_back(left);
+            }
+        }
+        if coord.y != 0 {
+            let up = Coord {
+                x: coord.x,
+                y: coord.y - 1,
+            };
+            if !is_explored(tiles, &explored, up.x, up.y) {
+                queue.push_back(up);
+            }
+        }
+        if coord.x < tiles.largest_x {
+            let right = Coord {
+                x: coord.x + 1,
+                y: coord.y,
+            };
+            if !is_explored(tiles, &explored, right.x, right.y) {
+                queue.push_back(right);
+            }
+        }
+        if coord.y < tiles.largest_y {
+            let down = Coord {
+                x: coord.x,
+                y: coord.y + 1,
+            };
+            if !is_explored(tiles, &explored, down.x, down.y) {
+                queue.push_back(down);
             }
         }
     }
@@ -99,30 +170,32 @@ fn generate_map(coords: &[Coord]) -> TileGrid {
         let largest_y = 99999;
     */
     let mut tiles = TileGrid {
-        tiles: vec![false; (largest_x + 1) * (largest_y + 1)],
+        tiles: vec![Tile::UNKNOWN; (largest_x + 2) * (largest_y + 2)],
         largest_x,
         largest_y,
     };
     let mut prev = coords[0];
-    set_tile(&mut tiles, prev.x, prev.y);
+    set_tile(&mut tiles, prev.x, prev.y, Tile::VALID);
     for coord in coords.iter().skip(1) {
         green_line(&mut tiles, &prev, coord);
-        print_grid(&tiles);
+        //print_grid(&tiles);
         prev = *coord;
     }
     green_line(&mut tiles, &prev, &coords[0]);
-    print_grid(&tiles);
+    println!("lines done");
+    //print_grid(&tiles);
     fill_grid(&mut tiles);
+    //print_grid(&tiles);
     tiles
 }
 
-fn all_red_or_green(tiles: &TileGrid, a: &Coord, b: &Coord) -> bool {
+fn all_red_or_green(tiles: &TileGrid, bools: &[bool], a: &Coord, b: &Coord) -> bool {
     let min_y = a.y.min(b.y);
     let max_y = a.y.max(b.y);
     let min_x = a.x.min(b.x);
     let max_x = a.x.max(b.x);
     for y in min_y..=max_y {
-        if !all_set(tiles, min_x, max_x, y) {
+        if !all_set(tiles, bools, min_x, max_x, y) {
             return false;
         }
     }
@@ -131,10 +204,13 @@ fn all_red_or_green(tiles: &TileGrid, a: &Coord, b: &Coord) -> bool {
 
 fn print_grid(tiles: &TileGrid) {
     println!();
-    for y in 0..=tiles.largest_y {
-        for x in 0..=tiles.largest_x {
-            if get_tile(tiles, x, y) {
+    for y in 0..=tiles.largest_y + 1 {
+        for x in 0..=tiles.largest_x + 1 {
+            let tile = get_tile(tiles, x, y);
+            if tile == Tile::VALID {
                 print!("#");
+            } else if tile == Tile::UNKNOWN {
+                print!("?");
             } else {
                 print!(".");
             }
@@ -144,17 +220,68 @@ fn print_grid(tiles: &TileGrid) {
     println!();
 }
 
+fn translate_coords(coords: &[Coord]) -> (HashMap<Coord, Coord>, Vec<Coord>) {
+    let mut temp: Vec<usize> = coords
+        .iter()
+        .map(|c| c.x)
+        .collect::<HashSet<usize>>()
+        .into_iter()
+        .collect();
+    temp.sort();
+    let x_lookup: HashMap<usize, usize> = temp
+        .into_iter()
+        .enumerate()
+        .map(|(i, x)| (x, (i + 1) * 3))
+        .collect();
+    let mut temp: Vec<usize> = coords
+        .iter()
+        .map(|c| c.y)
+        .collect::<HashSet<usize>>()
+        .into_iter()
+        .collect();
+    temp.sort();
+    let y_lookup: HashMap<usize, usize> = temp
+        .into_iter()
+        .enumerate()
+        .map(|(i, y)| (y, (i + 1) * 3))
+        .collect();
+    (
+        coords
+            .iter()
+            .map(|c| {
+                (
+                    Coord {
+                        x: *x_lookup.get(&c.x).unwrap(),
+                        y: *y_lookup.get(&c.y).unwrap(),
+                    },
+                    *c,
+                )
+            })
+            .collect(),
+        coords
+            .iter()
+            .map(|c| Coord {
+                x: *x_lookup.get(&c.x).unwrap(),
+                y: *y_lookup.get(&c.y).unwrap(),
+            })
+            .collect(),
+    )
+}
+
 fn part2(input: &str) -> usize {
     let coords = parse_input(input);
+    let (translation, coords) = translate_coords(&coords);
     let tiles = generate_map(&coords);
-    print_grid(&tiles);
+    let bools = to_bools(&tiles);
     println!("Map generated");
     let mut largest = 0;
     for (i, a) in coords.iter().enumerate() {
         println!("{}", i);
         for b in coords.iter().skip(i) {
-            let area = area(a, b);
-            if area > largest && all_red_or_green(&tiles, a, b) {
+            let translated_a = translation.get(a).unwrap();
+            let translated_b = translation.get(b).unwrap();
+            let area = area(translated_a, translated_b);
+            if area > largest && all_red_or_green(&tiles, &bools, a, b) {
                 largest = area;
             }
         }
@@ -214,6 +341,6 @@ mod tests {
     #[test]
     fn basic_test_part2() {
         assert_eq!(part2(BASIC_INPUT), 24);
-        // assert_eq!(part2(CONCAVE_INPUT), 15);
+        assert_eq!(part2(CONCAVE_INPUT), 25);
     }
 }
